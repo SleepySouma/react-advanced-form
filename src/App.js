e54da974f35162b914d370c44fbb33e831abf078
Soumaya <soumaya-lh@hotmail.com>
2022-03-22 16:09:33 +0100

    added axios (doesnt work)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 import { AdvancedForm } from './components/forms/AdvancedForm'
+import axios from 'axios';
 
 export default function App() {
   const [formValues, setFormValues] = useState([])
   const agentName = "Agent";
+  
+  //@mantis
+  const mantisApiURL = 'http://localhost:8069/mantisbt/api/rest/issues/';
+  const apiKey = 'Aqn6CisQ5YwLRuX2lTH8zLQOEYz-LeAj';
+  
 
   const handleSubmit = async (values, { setSubmitting }) => {
     setSubmitting(true)
     setFormValues(values)
     await new Promise((r) => setTimeout(r, 1000))
+    await postSubmission()
     setSubmitting(false)
   }
 
   const formSchema = [
-    { name: 'name', label: 'Nom', componentType: 'text', required: true },
+    { name: 'name', label: 'Nom - Provisoire', componentType: 'text', required: true },
     {
       name: 'category',
       label: 'Catégorie',
@@ -55,6 +62,60 @@ export default function App() {
         }
       ]
   }
+
+  //-------------------------------AXIOS PART------------------------------------------------//
+  
+  const postSubmission = async () => {
+
+    //@mantis 
+    const payloadMantis = {
+      ...formIssue,
+    }
+    //@mantisMonitoring
+    const payloadMonitor = {
+      ...formMonitoring
+    }
+
+    //@mantis
+    const resultMantis = await axios.post(mantisApiURL, 
+      payloadMantis,
+      {
+        headers: {
+          'Authorization': `${apiKey}`,
+          'content-type': 'application/json',
+        }
+      });
+
+      console.log(resultMantis);
+
+    /*how to get resulted issue's id:
+    let text = '{"issue":{"id":47,';
+    const myArray = text.split(",");
+    let result = myArray[0].substring(15);
+    */
+    const resultRawData = resultMantis.data;
+    console.log(resultRawData.issue.id); //for when the .data is returned as JSON type
+    //const resultCropped = resultRawData.split(","); //for when the .data is returned as a string
+    //const resultID = resultCropped[0].substring(15);
+    const resultID = resultRawData.issue.id;
+      
+    //@mantisMonitoring URL
+    const monitorApiURL = 'http://localhost:8069/mantisbt/api/rest/issues/' + resultID + '/monitors';
+
+    const resultMonitor = await axios.post(monitorApiURL, 
+      payloadMonitor,
+      {
+        headers: {
+          'Authorization': `${apiKey}`,
+          'content-type': 'application/json',
+        }
+      });//.catch
+
+      console.log(resultMonitor);
+      
+  };
+
+
 
   return ( //comment outside jsx elmt
     <>
